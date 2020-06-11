b9a1a741ef9a26c9a8e9f303cf83bae8aaf96f32
Antonio Jackson <justantonio89@gmail.com>
2020-06-10 19:24:54 -0700

    first commit

---
+++ src/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+// Getting all
+router.get(`/`, (req, res) => {
+    
+})
+// Getting one
+router.get(`/:id`, (req, res) => {
+    
+})
+// Creating one
+router.get(`/:id`, (req, res) => {
+    
+})
+// Updating one
+router.get(`/:id`, (req, res) => {
+    
+})
+// Deleting one
+router.get(`/:id`, (req, res) => {
+    
+})
+
+
+module.exports = router 
